b05fac2deed049d982a04ffbfed89de517ac3cac
gaurav <g8111997@gmail.com>
2026-05-15 01:27:01 +0530

    Initial Setup

---
+++ apps/dashboard-mfe/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), federation({
+    name: "dashboard",
+    filename: "remoteEntry.js",
+    exposes: {
+      "./Dashboard": "./src/App"
+    },
+    shared: ["react", "react-dom"]
+  })],
+})
---
+++ apps/shell/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), federation({
+    remotes: {
+      dashboard: "http://localhost:3002/assets/remoteEntry.js"
+    },
+    shared: ["react", "react-dom"]
+  })],
+})
