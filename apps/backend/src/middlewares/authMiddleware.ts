480d8956e5a8d4de9606cbf90d31e0399bf04bea
gaurav <g8111997@gmail.com>
2026-05-15 19:37:59 +0530

    add session schema, authValidation, authmiddleware

---
+++ apps/backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken"
+
+import type {
+    Request,
+    Response,
+    NextFunction
+} from "express"
+
+export const protect = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.sendStatus(401)
+    }
+
+    const token = authHeader.split(" ")[1]
+
+    try {
+
+        const decoded = jwt.verify(token!, process.env.JWT_SECRET!)
+
+        //@ts-ignore
+        req.user = decoded;
+
+        next()
+
+    } catch {
+
+        res.sendStatus(403)
+    }
+}
